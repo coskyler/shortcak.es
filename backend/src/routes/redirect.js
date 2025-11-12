4716df771c8f8567ba935b62d1ea945c5f978419
Skyler Quinby <skylerpquinby@gmail.com>
2025-11-12 17:40:48 -0500

    Install type definitions, change js files to ts

--- backend/src/routes/redirect.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-router.get("/:slug", async (req, res) => {
-    console.log('redirecting');
-    res.status(302).send("This is a redirect via: " + req.params.slug);
-})
-
-export default router;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,8 @@
 import express from "express";
-import linksRouter from "./routes/links.js";
-import analyticsRouter from "./routes/analytics.js";
-import aggregatesRouter from "./routes/aggregates.js";
-import redirectRouter from "./routes/redirect.js";
+import linksRouter from "./routes/links";
+import analyticsRouter from "./routes/analytics";
+import aggregatesRouter from "./routes/aggregates";
+import redirectRouter from "./routes/redirect";
 
 const app = express();
 app.use(express.json());
@@ -13,4 +13,5 @@ app.use("/api/aggregates", aggregatesRouter);
 app.use("/r", redirectRouter);
 
 app.listen(80);
+
 export default app;
